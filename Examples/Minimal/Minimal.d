bcbb022a93fa97395ac49201b1520edf15fa7e2c
Federico Santamorena <federico@santamorena.me>
2019-06-17 01:24:11 +0200

    fixes

--- Examples/Minimal/Minimal.d
+++ Examples/Minimal/Minimal.d
@@ -9,9 +9,8 @@ int main(string[] args)
     immutable(string) title = "Minimal Dhanos example";
     immutable(string) dhanos_project_path = dirName(dirName(dirName(absolutePath(buildNormalizedPath(args[0])))));
     immutable(string) url = buildPath("file:"~dhanos_project_path~"/LICENSE");
-    writeln("file:///"~dhanos_project_path~"/LICENSE");
-    int width = 800;
-    int height = 450;
-    bool resizable = true;
+    immutable int width = 800;
+    immutable int height = 450;
+    immutable bool resizable = true;
     return Dhanos.launch(title,url, width, height, resizable);
 }
