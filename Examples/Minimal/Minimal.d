b859194203e5a59f13ff115557f5d246ca10334c
Federico Santamorena <federico@santamorena.me>
2019-06-17 01:27:30 +0200

    fixes

--- Examples/Minimal/Minimal.d
+++ Examples/Minimal/Minimal.d
@@ -5,9 +5,8 @@ import Dhanos : Dhanos;
 
 int main(string[] args)
 {
-    import std.stdio : writeln;
     immutable(string) title = "Minimal Dhanos example";
-    immutable(string) dhanos_project_path = dirName(dirName(dirName(absolutePath(buildNormalizedPath(args[0])))));
+    immutable(string) dhanos_project_path = dirName(dirName(dirName(dirName(dirName(absolutePath(buildNormalizedPath(args[0])))))));
     immutable(string) url = buildPath("file:"~dhanos_project_path~"/LICENSE");
     immutable int width = 800;
     immutable int height = 450;
