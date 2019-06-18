455124447613580acd807cc24093dfdb9aac8f23
Federico Santamorena <federico@santamorena.me>
2019-06-18 05:04:44 +0200

    webview_init

--- Source/Dhanos.d
+++ Source/Dhanos.d
@@ -18,26 +18,82 @@ version (linux)
     extern (C) void gtk_widget_show_all(GtkWidget*);
 
     // GTK window
-    extern (C) struct GtkWindow;
-    //extern (C) GtkWindow* GTK_WINDOW(GtkWidget*);
-    //extern (C) GtkWindow* gtk_window_new(int);
-    extern (C) void gtk_window_set_title(GtkWindow*, const char*);
-    extern (C) void gtk_window_set_default_size(GtkWindow*, int, int);
-    extern (C) void gtk_window_set_resizable(GtkWindow*, bool);
-    extern (C) void gtk_window_set_decorated(GtkWindow*, bool);
-
-    extern (C) int gtk_main_iteration_do(int);
-
-    extern (C) void gtk_window_fullscreen(GtkWindow*);
-    extern (C) void gtk_window_unfullscreen(GtkWindow*);
-
     enum GtkWindowType
     {
         GTK_WINDOW_TOPLEVEL,
         GTK_WINDOW_POPUP
     };
+    enum GtkWindowPosition
+    {
+        GTK_WIN_POS_NONE,
+        GTK_WIN_POS_CENTER,
+        GTK_WIN_POS_MOUSE,
+        GTK_WIN_POS_CENTER_ALWAYS,
+        GTK_WIN_POS_CENTER_ON_PARENT
+    }
+
+    extern (C) struct GtkWindow;
+    extern (C) GtkWindow* GTK_WINDOW(GtkWidget*);
+    extern (C) GtkWindow* gtk_window_new(GtkWindowType);
+    extern (C) void gtk_window_set_title(GtkWindow*, const char*);
+    extern (C) void gtk_window_set_default_size(GtkWindow*, int, int);
+    extern (C) void gtk_window_set_resizable(GtkWindow*, bool);
+    extern (C) void gtk_window_set_decorated(GtkWindow*, bool);
+    extern (C) void gtk_window_fullscreen(GtkWindow*);
+    extern (C) void gtk_window_unfullscreen(GtkWindow*);
+    extern (C) void gtk_window_set_position(GtkWindow*, GtkWindowPosition);
+
+   enum GConnectFlags
+   {
+       G_CONNECT_AFTER,
+       G_CONNECT_SWAPPED
+   };
+
+    extern (C) void g_signal_connect_data (void* instance, const char* detailed_signal, void* c_handler, void* data, void* destroy_data, GConnectFlags connect_flags);
+    alias g_signal_connect = g_signal_connect_data;
+    
+
+    extern (C) struct GtkAdjustment;
+    extern (C) GtkWidget *
+gtk_scrolled_window_new (GtkAdjustment *hadjustment,
+                         GtkAdjustment *vadjustment);
+
+    extern (C) struct GtkContainer;
+    extern (C) GtkContainer* GTK_CONTAINER(GtkWidget*);
+    extern (C) void gtk_container_add(GtkContainer* container, GtkWidget* widget);
+
+    extern (C) struct WebKitUserContentManager;
+
+    extern (C) GObject* G_OBJECT(GtkWidget*);
+    
+    extern (C) void* G_CALLBACK(void*);
+
+    extern (C) WebKitWebView* WEBKIT_WEB_VIEW(GtkWidget*);
+    extern (C) WebKitUserContentManager* webkit_user_content_manager_new();
+    extern (C) bool webkit_user_content_manager_register_script_message_handler(
+            WebKitUserContentManager* manager, const char* name);
+    extern (C) struct WebKitSettings;
+    extern (C) struct WebKitWebView;
+
+    extern (C) int gtk_main_iteration_do(int);
+    extern (C) WebKitSettings* webkit_web_view_get_settings(WebKitWebView* web_view);
+    extern (C) void webkit_settings_set_enable_write_console_messages_to_stdout(
+            WebKitSettings* settings, bool enabled);
+    extern (C) void webkit_settings_set_enable_developer_extras(
+            WebKitSettings* settings, bool enabled);
+    extern (C) struct GCancellable;
+    extern (C) struct GAsyncResult;
+    extern (C) struct GObject;
+    alias GAsyncReadyCallback = void function(GObject* source_object,
+            GAsyncResult* res, void* user_data);
+    extern (C) void webkit_web_view_run_javascript(WebKitWebView* web_view, const char* script,
+            GCancellable* cancellable, GAsyncReadyCallback callback, void* user_data);
+
+    extern (C) GtkWidget* webkit_web_view_new_with_user_content_manager(
+            WebKitUserContentManager* user_content_manager);
+    extern (C) void webkit_web_view_load_uri(WebKitWebView* web_view, const char* uri);
 }
-//extern (C) enum GtkWindowType;
+// extern (C) enum GtkWindowType;
 
 struct webview_priv
 {
@@ -79,9 +135,57 @@ extern (C) void webview_exit(webview* w);
 alias webview_dispatch_fn = void function(webview* w, void* arg);
 extern (C) void webview_dispatch(webview* w, webview_dispatch_fn fn, void* arg);
 
-
 import std.stdio : writeln;
 import std.string : fromStringz;
+
+void webview_destroy_cb(GtkWidget* widget, void* arg)
+{
+    webview* w = cast(webview*) arg;
+    version (linux)
+    {
+        w.priv.should_exit = 1;
+    }
+}
+
+extern (C) struct WebKitJavascriptResult;
+enum WebKitLoadEvent
+{
+    WEBKIT_LOAD_STARTED,
+    WEBKIT_LOAD_REDIRECTED,
+    WEBKIT_LOAD_COMMITTED,
+    WEBKIT_LOAD_FINISHED
+};
+
+void external_message_received_cb(WebKitUserContentManager* m, WebKitJavascriptResult* r, void* arg)
+{
+  
+    // webview * w = cast(webview *) arg;
+    // if (w.external_invoke_cb == null)
+    // {
+    //     return;
+    // }
+    // JSGlobalContextRef context = webkit_javascript_result_get_global_context(r);
+    // JSValueRef value = webkit_javascript_result_get_value(r);
+    // JSStringRef js = JSValueToStringCopy(context, value, NULL);
+    // size_t n = JSStringGetMaximumUTF8CStringSize(js);
+    // char* s = g_new(char, n);
+    // JSStringGetUTF8CString(js, s, n);
+    // w.external_invoke_cb(w, s);
+    // JSStringRelease(js);
+    // g_free(s);
+}
+
+
+void webview_load_changed_cb(WebKitWebView* wwv, WebKitLoadEvent event, void* arg)
+{
+    webview* w = cast(webview*)arg;
+    
+    if (event == WebKitLoadEvent.WEBKIT_LOAD_FINISHED)
+    {
+        w.priv.ready = 1;
+    }
+}
+
 
 class Dhanos
 {
@@ -89,6 +193,23 @@ class Dhanos
     // int should_exit;
     // GAsyncQueue* queue;
     // GtkWidget* window;
+
+    static immutable(string) DEFAULT_URL = "data:text/"                          ~
+    "html,%3C%21DOCTYPE%20html%3E%0A%3Chtml%20lang=%22en%22%3E%0A%3Chead%3E%"    ~
+    "3Cmeta%20charset=%22utf-8%22%3E%3Cmeta%20http-equiv=%22X-UA-Compatible%22%" ~
+    "20content=%22IE=edge%22%3E%3C%2Fhead%3E%0A%3Cbody%3E%3Cdiv%20id=%22app%22%" ~
+    "3E%3C%2Fdiv%3E%3Cscript%20type=%22text%2Fjavascript%22%3E%3C%2Fscript%3E%"  ~
+    "3C%2Fbody%3E%0A%3C%2Fhtml%3E";
+
+    static immutable(string) checkURL(immutable(string) url)
+    {
+        if (url == null || url.length == 0)
+        {
+            return DEFAULT_URL;
+        }
+        return url;
+    }
+
 
     string title;
     string url;
@@ -271,16 +392,12 @@ class Dhanos
     //     // return 0;
     // }
 private:
-    void function(immutable(string)) callback;
-
-
+void function(immutable(string)) callback;
 
 public:
-
-
-    void raw_callback(webview* wv, const char* arg)
-    in(wv != null)
-    in(arg != null)
+void raw_callback(webview* wv, const char* arg)
+in(wv != null)
+in(arg != null)
     {
         if (wv == null)
             throw new Exception("Webview in JS=>D raw callback is null!");
@@ -311,6 +428,65 @@ public:
     //         writeln("noop_callback");
 
     // }
+
+    int webview_init(webview* w)
+    {
+        if (gtk_init_check(null, null) == false)
+        {
+            return -1;
+        }
+
+        w.priv.ready = 0;
+        w.priv.should_exit = 0;
+        w.priv.queue = g_async_queue_new();
+        w.priv.window = cast(GtkWidget*) gtk_window_new(GtkWindowType.GTK_WINDOW_TOPLEVEL);
+        gtk_window_set_title(cast(GtkWindow*)w.priv.window, w.title);
+
+        if (w.resizable)
+        {
+            gtk_window_set_default_size(cast(GtkWindow*)(w.priv.window), w.width, w.height);
+        }
+        else
+        {
+            gtk_widget_set_size_request(w.priv.window, w.width, w.height);
+        }
+        gtk_window_set_resizable(cast(GtkWindow*)(w.priv.window), !!w.resizable);
+        gtk_window_set_position(cast(GtkWindow*)(w.priv.window), GtkWindowPosition.GTK_WIN_POS_CENTER);
+
+        w.priv.scroller = gtk_scrolled_window_new(null, null);
+        gtk_container_add(cast(GtkContainer*)(w.priv.window), w.priv.scroller);
+
+        WebKitUserContentManager* m = webkit_user_content_manager_new();
+        webkit_user_content_manager_register_script_message_handler(m, "external");
+        g_signal_connect(cast(void*)m, cast(const char*)toStringz("script-message-received::external"), cast(void*)&external_message_received_cb, cast(void*)w,null,GConnectFlags.G_CONNECT_AFTER);
+
+        w.priv.webview = webkit_web_view_new_with_user_content_manager(m);
+        webkit_web_view_load_uri(cast(WebKitWebView*)(w.priv.webview), toStringz(checkURL(fromStringz(w.url).idup)));
+        g_signal_connect(cast(void*)w.priv.webview, cast(const char*)toStringz("load-changed"), &webview_load_changed_cb, cast(void*)w,null,GConnectFlags.G_CONNECT_AFTER);
+        gtk_container_add(cast(GtkContainer*)(w.priv.scroller), w.priv.webview);
+
+        debug
+        {
+            WebKitSettings* settings = webkit_web_view_get_settings(cast(WebKitWebView*)(w.priv.webview));
+            webkit_settings_set_enable_write_console_messages_to_stdout(settings, true);
+            webkit_settings_set_enable_developer_extras(settings, true);
+        }
+        else
+        {
+            g_signal_connect(G_OBJECT(w.priv.webview), "context-menu",
+                    G_CALLBACK(webview_context_menu_cb), w);
+        }
+
+        gtk_widget_show_all(w.priv.window);
+
+        webkit_web_view_run_javascript(cast(WebKitWebView*)(w.priv.webview),
+                "window.external={invoke:function(x){" ~ "window.webkit.messageHandlers.external.postMessage(x);}}",
+                null, null, null);
+
+        auto d = toStringz("destroy");
+        g_signal_connect(w.priv.window, cast(char*)d, cast(void*)&webview_destroy_cb, cast(void*)w,null,GConnectFlags.G_CONNECT_AFTER);
+        return 0;
+    }
 
     this(immutable(string) title, immutable(string) url, int width, int height, bool resizable)
     {
