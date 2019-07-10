8ceb75c12daf03f1f4511a55bb13cf943930ea6f
Federico Santamorena <federico@santamorena.me>
2019-07-10 02:17:23 +0200

    borderless fixes

--- Examples/Borderless/Borderless.d
+++ Examples/Borderless/Borderless.d
@@ -1,10 +1,11 @@
 module Examples.Minimal;
 
 import std.path : dirName, buildNormalizedPath, absolutePath, buildPath;
-import Dhanos : Dhanos;
 
-import Dhanos : webview;
-   import std.stdio : writeln;
+
+import DhanosInterface : DhanosInterface;
+import Dhanos : getNewPlatformInstance;
+import std.stdio : writeln;
 
 void callback(immutable(string) value)
 {
@@ -12,18 +13,21 @@ void callback(immutable(string) value)
     
 }
 
+
+
 int main(string[] args)
 {
     immutable(string) title = "Borderless Window";
-    immutable(string) dhanos_project_path = dirName(dirName(dirName(absolutePath(buildNormalizedPath(args[0])))));
+    immutable(string) dhanos_project_path = dirName(dirName(absolutePath(buildNormalizedPath(args[0]))));
     immutable(string) url = buildPath("file:" ~ dhanos_project_path ~ "/index.html");
     immutable int width = 800;
     immutable int height = 250;
     immutable bool resizable = false;
-    Dhanos d = new Dhanos(title, url, width, height, resizable);
-    d.setBorder(false);
+    DhanosInterface d = getNewPlatformInstance(title, url, width, height, resizable);
+   
+    //d.setBorder(false);
     auto f = &callback;
-    d.setJSCallback(f);
+    //d.setJSCallback(f);
     d.mainLoop();
     return 0;
 }
